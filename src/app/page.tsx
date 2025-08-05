e43c70df3f275f106c3f17c62b332400d047797f
Raitis <raitis.cakstins@madaracosmetics.com>
2025-08-05 11:12:19 +0300

    Commented out page sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import { Header } from "@/components/sections/Header";
 import { HeroSection } from "@/components/sections/HeroSection";
 import { ServicesSection } from "@/components/sections/ServicesSection";
-import { AboutSection } from "@/components/sections/AboutSection";
-import { LogoSection } from "@/components/sections/LogoSection";
-import { StatsSection } from "@/components/sections/StatsSection";
-import { TestimonialsSection } from "@/components/sections/TestimonialsSection";
-import { ProcessSection } from "@/components/sections/ProcessSection";
+// import { AboutSection } from "@/components/sections/AboutSection";
+// import { LogoSection } from "@/components/sections/LogoSection";
+// import { StatsSection } from "@/components/sections/StatsSection";
+// import { TestimonialsSection } from "@/components/sections/TestimonialsSection";
+// import { ProcessSection } from "@/components/sections/ProcessSection";
 import { PricingSection } from "@/components/sections/PricingSection";
 import { NewsletterSection } from "@/components/sections/NewsletterSection";
 import { FinalCTASection } from "@/components/sections/FinalCTASection";
@@ -17,11 +17,11 @@ export default function Home() {
       <Header />
       <HeroSection />
       <ServicesSection />
-      <AboutSection />
-      <LogoSection />
-      <StatsSection />
-      <TestimonialsSection />
-      <ProcessSection />
+      {/* <AboutSection /> */}
+      {/* <LogoSection /> */}
+      {/* <StatsSection /> */}
+      {/* <TestimonialsSection /> */}
+      {/* <ProcessSection /> */}
       <PricingSection />
       <NewsletterSection />
       <FinalCTASection />
